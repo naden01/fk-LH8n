a9ce833975ae3e85032147e6400dd502da3904a2
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-08-07 07:44:00 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_TECNO-LH8n-user
-add_lunch_combo omni_TECNO-LH8n-userdebug
-add_lunch_combo omni_TECNO-LH8n-eng
+add_lunch_combo twrp_LH8n-user
+add_lunch_combo twrp_LH8n-userdebug
+add_lunch_combo twrp_LH8n-eng
